878d26439da7acd6c9f3c4779ba247f2afefb1b3
Yash Chakrabarti <yashchakrabarti@gmail.com>
2019-02-08 03:07:56 +0000

    Work upto Chapter 9 Exercises

--- rango/models.py
+++ rango/models.py
@@ -3,17 +3,16 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
 
-
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug=models.SlugField(unique=True)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug=slugify(self.name)
+        self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
     class Meta:
@@ -32,13 +31,12 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
-class UserProfile(models.Model):
-    user=models.OneToOneField(User)
 
-    website=models.URLField(blank=True)
-    picture=models.ImageField(upload_to="profile_images", blank=True)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to="profile_images", blank=True)
 
     def __str__(self):
         return self.user.username
-
-
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -128,3 +128,5 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+LOGIN_URL = "/rango/login/"
